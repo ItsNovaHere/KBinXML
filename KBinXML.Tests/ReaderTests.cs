55c04150045b8f703d0628d05d3eb81a3849233a
ItsNovaHere <theawesome624@hotmail.com>
2021-04-06 20:14:24 -0500

    Add tests project

---
+++ KBinXML.Tests/ReaderTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace KBinXML.Tests {
+
+	public class ReaderTests {
+
+		private readonly ITestOutputHelper _testOutputHelper;
+
+		public ReaderTests(ITestOutputHelper testOutputHelper) {
+			_testOutputHelper = testOutputHelper;
+		}
+
+		[Fact]
+		public void ProperDecode() {
+			using var file = File.OpenRead("test.kbin");
+			
+			using var reader = new Reader(file);
+			var document = reader.GetDocument();
+			
+			Assert.True(XNode.DeepEquals(XDocument.Load("test.xml"), document));
+		}
+		
+	}
+
+}
